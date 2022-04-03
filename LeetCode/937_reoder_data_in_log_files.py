df9ce3eba2a3116e2a18c63b6e76ca1e0b6913dc
dohelee <leeworld9@gmail.com>
2022-04-04 00:36:42 +0900

    add leetcode 937

---
+++ LeetCode/937_reoder_data_in_log_files.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split(" ")[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+
+        # referenc : https://gorokke.tistory.com/38
+        # 2개의 키를 람다 표현식으로 정렬
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters + digits
+
+
+sol = Solution()
+log = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+print(sol.reorderLogFiles(log))
