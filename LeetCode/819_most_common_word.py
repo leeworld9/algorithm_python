b652c67bb419518ba23432e2dd5594a1c90ffd07
dohelee <leeworld9@gmail.com>
2022-04-04 04:16:49 +0900

    add: leetcode 819

---
+++ LeetCode/819_most_common_word.py
@@ -0,0 +1,18 @@
+import collections
+import re
+from typing import List
+
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+            .lower().split() if word not in banned]
+
+        counts = collections.Counter(words)
+        # 가장 흔하게 등장하는 단어의 첫번째 인덱스리턴
+        return counts.most_common(1)[0][0]
+
+sol = Solution()
+paragraph = "a, a, a, a, b,b,b,c, c"
+banned = ["a"]
+print(sol.mostCommonWord(paragraph, banned))
