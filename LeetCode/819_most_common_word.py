af6bf0922466d859ac638b5888f26a5f8fad0026
dohelee <leeworld9@gmail.com>
2022-04-04 05:57:29 +0900

    fix: leetcode 819

--- LeetCode/819_most_common_word.py
+++ LeetCode/819_most_common_word.py
@@ -8,6 +8,7 @@ class Solution:
         words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
             .lower().split() if word not in banned]
 
+        # Counter 모듈 사용
         counts = collections.Counter(words)
         # 가장 흔하게 등장하는 단어의 첫번째 인덱스리턴
         return counts.most_common(1)[0][0]
