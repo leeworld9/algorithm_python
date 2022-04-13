d49b001d620105b5951091a6f32d1a679b93c61c
dohelee <leeworld9@gmail.com>
2022-04-13 19:54:25 +0900

    add : leetcode 15

---
+++ LeetCode/15_3sum.py
@@ -0,0 +1,44 @@
+import time
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            # 중복된 값 건너뛰기
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            # 간격을 좁혀가며 합 `sum` 계산
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    # `sum = 0`인 경우이므로 정답 및 스킵 처리
+                    results.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+
+        return results
+
+
+"""
+sol = Solution()
+nums = [34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49]
+#nums = [0, 0, 0, 0]
+start = time.time()
+print(sol.threeSum(nums))
+print("time : {t} sec".format(t=time.time()-start))
+"""
