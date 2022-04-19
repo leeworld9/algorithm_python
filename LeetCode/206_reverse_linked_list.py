843c8cca1cb934d5e91baae8ed56e75ed61990f3
dohelee <leeworld9@gmail.com>
2022-04-19 17:26:32 +0900

    FIX : change logic

--- LeetCode/206_reverse_linked_list.py
+++ LeetCode/206_reverse_linked_list.py
@@ -9,10 +9,11 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def reverse(node: ListNode, prev: ListNode = None):
-            if not node:
-                return prev
+        node, prev = head, None
+
+        while node:
             next, node.next = node.next, prev
-            return reverse(next, node)
-        return reverse(head)
+            prev, node = node, next
+
+        return prev
 
