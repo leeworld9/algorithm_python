d150478114310de65dc07dfdfff76a0b3e84b22e
dohelee <leeworld9@gmail.com>
2022-04-19 17:22:39 +0900

    FIX : delete comment

--- LeetCode/206_reverse_linked_list.py
+++ LeetCode/206_reverse_linked_list.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from snippet import LinkedList
+
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -16,20 +16,3 @@ class Solution:
             return reverse(next, node)
         return reverse(head)
 
-
-"""
-sol = Solution()
-li = LinkedList()
-
-li.append(1)
-li.append(2)
-li.append(3)
-li.append(4)
-li.append(5)
-
-#cur = li.head
-cur = sol.reverseList(li.head)
-while cur is not None:
-    print(cur.val)
-    cur = cur.next
-"""
